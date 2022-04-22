d1a094441a51350650a061cd2264d0f1c1f3c4c8
ERLMKea <erlm@kea.dk>
2022-04-22 09:10:34 +0200

    test exception lambda

--- src/test/java/com/example/xsmittetryk05/XSmittetryk05ApplicationTests.java
+++ src/test/java/com/example/xsmittetryk05/XSmittetryk05ApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.xsmittetryk05;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.Arrays;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
 class XSmittetryk05ApplicationTests {
 
     @Test
@@ -24,4 +25,41 @@ class XSmittetryk05ApplicationTests {
     void test2() {
         assertEquals("ABC", "ABC");
     }
+
+    @Test
+    void testArr() {
+        int[] numbers = {12,3,4,1};
+        int[] expected = {1,3,4,12};
+        Arrays.sort(numbers);
+        assertEquals(expected, numbers);
+    }
+
+    @Test
+    void testArr2() {
+        int[] numbers = {12,3,4,1};
+        int[] expected = {1,3,4,12};
+        Arrays.sort(numbers);
+        assertArrayEquals(expected, numbers);
+    }
+
+    @Test
+    void testArrNullPointerExcept() {
+        int[] numbers = null;
+        int[] expected = {1,3,4,12};
+        try {
+            Arrays.sort(numbers);
+
+        } catch (NullPointerException nullerr) {
+            System.out.println("Nullexp err=" +  nullerr);
+        }
+    }
+
+    @Test
+    void testArithmException() {
+        assertThrows(ArithmeticException.class, () -> {
+            int result = 3/0;
+        });
+    }
+
+
 }
