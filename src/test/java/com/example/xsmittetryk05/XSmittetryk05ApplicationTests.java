66ada536a9ed04f514705f0a603e831f7c1355f8
ERLMKea <erlm@kea.dk>
2022-04-22 08:52:25 +0200

    simpel test

--- src/test/java/com/example/xsmittetryk05/XSmittetryk05ApplicationTests.java
+++ src/test/java/com/example/xsmittetryk05/XSmittetryk05ApplicationTests.java
@@ -3,6 +3,9 @@ package com.example.xsmittetryk05;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class XSmittetryk05ApplicationTests {
 
@@ -10,4 +13,15 @@ class XSmittetryk05ApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void test() {
+        if (3>4) {
+            fail("det går slet ikke");
+        }
+    }
+
+    @Test
+    void test2() {
+        assertEquals("ABC", "ABC");
+    }
 }
